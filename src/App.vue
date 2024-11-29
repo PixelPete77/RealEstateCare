ae3bd467fa2da059ea06649dc87aeebbd5eac784
Pieter Bas Lugthart <pieterbas@80db.nl>
2024-11-29 16:29:19 +0100

    Add Ionic to the project

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { IonApp, IonRouterOutlet } from '@ionic/vue';
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <img alt="Real Estate Care logo" class="logo" src="./assets/images/RealEstateCare_logo_v.svg" width="240" height="152" />
-      <HelloWorld msg="Hello world!" />
-    </div>
-  </header>
-
-  <main>
-    
-  </main>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
 </template>
-
-<style scoped>
-.logo {
-  display: block;
-  margin: 0 auto 3rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-  }
-
-  header .wrapper {
-    display: flex;
-    flex-direction: column;
-  }
-}
-</style>
