d8350bb5b86669fe61d6e39c14a10d9fcbaaa2c7
Pieter Bas Lugthart <pieterbas@80db.nl>
2024-12-05 15:17:43 +0100

    Main menu active link style

--- src/components/mainmenu/MainMenuItem.vue
+++ src/components/mainmenu/MainMenuItem.vue
@@ -26,6 +26,7 @@
     }
 
     a {
+        position: relative;
         color: #fff;
         display: flex;
         flex-direction: column;
@@ -34,10 +35,34 @@
         line-height: 1.2;
         padding: .5rem;
         text-align: center;
+        text-decoration: none;
+    }
+
+    a::after {
+        position: absolute;
+        left: calc(50% - 5px);
+        bottom: -6px;
+        inline-size: 10px;
+        block-size: 10px;
+        background-color: currentColor;
+        content: '';
+        transform: rotate(45deg) translateY(100%);
+        transition: transform .2s ease;
+        transform-origin: center;
+    }
+
+    a.router-link-active::after {
+        transform: rotate(45deg) translateY(0);
     }
 
     :deep(svg) {
         inline-size: 1.5rem;
         margin-block-end: .25rem;
+        opacity: .67;
+        transition: opacity .2s ease;
+    }
+
+    a.router-link-active :deep(svg) {
+        opacity: 1;
     }
 </style>
