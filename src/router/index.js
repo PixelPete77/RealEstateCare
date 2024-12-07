708b3e92317d43cd32299e95f67490f3fef84b30
Pieter Bas Lugthart <pieterbas@80db.nl>
2024-12-07 22:41:11 +0100

    Change page title based on route

--- src/router/index.js
+++ src/router/index.js
@@ -1,63 +1,67 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 
 const routes = [
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      path: '/completed',
-      name: 'Completed inspections',
-      component: () => import('@/views/CompletedInspectionsPage.vue'),
-      meta: { layout: 'default' },
-    },
-    {
-      path: '/help',
-      name: 'Help',
-      component: () => import('@/views/HelpPage.vue'),
-      meta: { layout: 'default' },
-    },
-    {
-      path: '/home',
-      name: 'Home',
-      component: () => import('@/views/DashboardPage.vue'),
-      meta: { layout: 'default' },
-    },
-    {
-      path: '/knowledge-base',
-      name: 'Knowledge base',
-      component: () => import('@/views/KnowledgeBasePage.vue'),
-      meta: { layout: 'default' },
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: () => import('@/views/LoginPage.vue'),
-      meta: { layout: 'login' },
-    },
-    {
-      path: '/scheduled',
-      name: 'Scheduled inspections',
-      component: () => import('@/views/ScheduledInspectionsPage.vue'),
-      meta: { layout: 'default' },
-    },
-    {
-      path: '/settings',
-      name: 'Settings',
-      component: () => import('@/views/SettingsPage.vue'),
-      meta: { layout: 'default' },
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'NotFound',
-      component: () => import('@/views/NotFoundPage.vue'),
-      meta: { layout: 'login' },
-    },
+  {
+    path: '/',
+    redirect: '/home',
+  },
+  {
+    path: '/completed',
+    name: 'Completed inspections',
+    component: () => import('@/views/CompletedInspectionsPage.vue'),
+    meta: { layout: 'default', title: 'Completed inspections' },
+  },
+  {
+    path: '/help',
+    name: 'Help',
+    component: () => import('@/views/HelpPage.vue'),
+    meta: { layout: 'default', title: 'Help' },
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('@/views/DashboardPage.vue'),
+    meta: { layout: 'default', title: 'Dashboard' },
+  },
+  {
+    path: '/knowledge-base',
+    name: 'Knowledge base',
+    component: () => import('@/views/KnowledgeBasePage.vue'),
+    meta: { layout: 'default', title: 'Knowledge base' },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/LoginPage.vue'),
+    meta: { layout: 'login', title: 'Login' },
+  },
+  {
+    path: '/scheduled',
+    name: 'Scheduled inspections',
+    component: () => import('@/views/ScheduledInspectionsPage.vue'),
+    meta: { layout: 'default', title: 'Scheduled inspections' },
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: () => import('@/views/SettingsPage.vue'),
+    meta: { layout: 'default', title: 'Help' },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('@/views/NotFoundPage.vue'),
+    meta: { layout: 'login' },
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+router.afterEach((to) => {
+  document.title = `${to.meta.title} | RealEstateCare` || 'RealEstateCare';
 });
 
 export default router;
