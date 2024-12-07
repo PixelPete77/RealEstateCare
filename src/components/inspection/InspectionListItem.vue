69c0b0d0aa662652a27088b8723df2e53f7c53e0
Pieter Bas Lugthart <pieterbas@80db.nl>
2024-12-07 22:24:53 +0100

    Inspections list item styling - responsive improvements

--- src/components/inspection/InspectionListItem.vue
+++ src/components/inspection/InspectionListItem.vue
@@ -29,26 +29,47 @@
     li {
         border-bottom: 1px solid hsl(0, 0%, 50%);
         display: flex;
-        align-items: center;
+        flex-direction: column;
         gap: 1rem;
         padding-block-end: 1.5rem;
     }
+
+    @media screen and (min-width: 30rem) {
+        li {
+            flex-direction: row;
+            align-items: end;
+        }
+    }
+
     div {
         flex: 1 1 auto;
         display: flex;
         flex-direction: column;
         gap: .5rem;
     }
-    div > * {
+
+    @media screen and (min-width: 48rem) {
+        div {
+            flex-direction: row;
+            flex-wrap: wrap;
+            gap: 1rem;
+        }
+    }
+
+    h2 {
+        flex: 1 1 100%;
         margin-block-end: 0;
     }
+
     span {
         display: block;
         opacity: .67;
     }
+
     ion-button {
         flex: 0 0 auto;
     }
+
     ion-button :deep(svg) {
         inline-size: 1rem;
         block-size: auto;
