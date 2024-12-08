a6ee92642163fef8747300910632080affe3afa2
Pieter Bas Lugthart <pieterbas@80db.nl>
2024-12-08 19:57:10 +0100

    Inspection page display data

--- src/views/InspectionPage.vue
+++ src/views/InspectionPage.vue
@@ -26,7 +26,6 @@
 
     // Check if inspection data is present when the component is mounted
     onMounted(() => {
-        console.log("Inspections? ", store.inspections.length);
         if (!store.inspections.length) {
             store.fetchInspections();
         } 
@@ -38,9 +37,11 @@
         <ion-content>
             <div class="wrapper">
                 <h1>Inspection #{{ route.params.id }}</h1>
+                <!-- Show loading indicator when fetching data -->
                 <div v-if="!store.inspections.length">
                     <p>Loading inspection...</p>
                 </div>
+                <!-- Inspection data -->
                 <div v-if="inspection">
                     <p>
                         <strong>Date:</strong><br>
@@ -55,12 +56,13 @@
                     </p>
                 </div>
                 <form v-if="inspection">
-                    <fieldset>
-                        <legend>Record damage</legend>
+                    <h2>Record damage</h2>
+                    <fieldset v-for="dmg in inspection.damage" :key="dmg.id">
+                        <legend>Damage {{ dmg.id }}</legend>
                         <ion-label>Date:</ion-label>
                         <ion-datetime-button datetime="datetime"></ion-datetime-button>
-                        <ion-input label="Location" label-placement="stacked" fill="outline" placeholder="Enter the location"></ion-input>
-                        <ion-select label="Type of damage" label-placement="stacked" fill="outline" placeholder="Select a type of damage">
+                        <ion-input label="Location" label-placement="stacked" fill="outline" placeholder="Enter the location" :value="dmg.location"></ion-input>
+                        <ion-select label="Type of damage" label-placement="stacked" fill="outline" placeholder="Select a type of damage" :value="dmg.type">
                             <ion-select-option value="intentional">Intentional</ion-select-option>
                             <ion-select-option value="wear">Wear</ion-select-option>
                             <ion-select-option value="force">Force</ion-select-option>
@@ -68,70 +70,73 @@
                             <ion-select-option value="calamity">Calamity</ion-select-option>
                             <ion-select-option value="other">Other</ion-select-option>
                         </ion-select>
-                        <ion-textarea label="Description" label-placement="stacked" fill="outline" placeholder="Enter a description of the damage"></ion-textarea>
-                        <ion-checkbox label-placement="end" :checked="inspection.damage.new">New damage</ion-checkbox>
-                        <ion-checkbox label-placement="end" :checked="inspection.damage.urgent">Urgent action required</ion-checkbox>
+                        <ion-textarea label="Description" label-placement="stacked" fill="outline" placeholder="Enter a description of the damage":value="dmg.description"></ion-textarea>
+                        <ion-checkbox label-placement="end" :checked="dmg.new">New damage</ion-checkbox>
+                        <ion-checkbox label-placement="end" :checked="dmg.urgent">Urgent action required</ion-checkbox>
                         <!-- TODO: add option to add photo's -->
 
                         <ion-modal :keep-contents-mounted="true">
-                            <ion-datetime id="datetime" presentation="date" :value="inspection.damage.date"></ion-datetime>
+                            <ion-datetime id="datetime" presentation="date" :value="dmg.date"></ion-datetime>
                         </ion-modal>
                     </fieldset>
-                    <fieldset>
-                        <legend>Record deferred maintenance</legend>
-                        <ion-input label="Location" label-placement="stacked" fill="outline" placeholder="Enter the location"></ion-input>
-                        <ion-select label="Type of maintenance" label-placement="stacked" fill="outline" placeholder="Select a type of maintenance">
+                    <h2>Record deferred maintenance</h2>
+                    <fieldset v-for="mntnc in inspection.maintenance" :key="mntnc.id">
+                        <legend>Deferred maintenance {{ mntnc.id }}</legend>
+                        <ion-input label="Location" label-placement="stacked" fill="outline" placeholder="Enter the location" :value="mntnc.location"></ion-input>
+                        <ion-select label="Type of maintenance" label-placement="stacked" fill="outline" placeholder="Select a type of maintenance" :value="mntnc.type">
                             <ion-select-option value="painting">Painting</ion-select-option>
                             <ion-select-option value="woodrot">Wood rot</ion-select-option>
                             <ion-select-option value="electricity">Electricity</ion-select-option>
                             <ion-select-option value="piping">Piping</ion-select-option>
                             <ion-select-option value="glass">Glass</ion-select-option>
                         </ion-select>
-                        <ion-select label="Cost indication" label-placement="stacked" fill="outline" placeholder="Select a cost indication">
+                        <ion-select label="Cost indication" label-placement="stacked" fill="outline" placeholder="Select a cost indication" :value="mntnc.costIndication">
                             <ion-select-option value="0-500">0 - 500</ion-select-option>
                             <ion-select-option value="500-1500">500 - 1.500</ion-select-option>
                             <ion-select-option value="1500+">1.500+</ion-select-option>
                         </ion-select>
-                        <ion-checkbox label-placement="end" :checked="inspection.maintenance.urgent">Urgent action required</ion-checkbox>
+                        <ion-checkbox label-placement="end" :checked="mntnc.urgent">Urgent action required</ion-checkbox>
                     </fieldset>
-                    <fieldset>
-                        <legend>Inspect technical installations</legend>
-                        <ion-input label="Location" label-placement="stacked" fill="outline" placeholder="Enter the location"></ion-input>
-                        <ion-select label="Type of installation" label-placement="stacked" fill="outline" placeholder="Select a type of installation">
+                    <h2>Inspect technical installations</h2>
+                    <fieldset v-for="instal in inspection.installations" :key="instal.id">
+                        <legend>Technical installation {{ instal.id }}</legend>
+                        <ion-input label="Location" label-placement="stacked" fill="outline" placeholder="Enter the location" :value="instal.location"></ion-input>
+                        <ion-select label="Type of installation" label-placement="stacked" fill="outline" placeholder="Select a type of installation" :value="instal.type">
                             <ion-select-option value="cooling">Cooling</ion-select-option>
                             <ion-select-option value="heating">Heating</ion-select-option>
                             <ion-select-option value="ventilation">Ventilation</ion-select-option>
                             <ion-select-option value="electricity">Electricity</ion-select-option>
                             <ion-select-option value="security">Security</ion-select-option>
                         </ion-select>
-                        <ion-input label="Reported outages" label-placement="stacked" fill="outline" placeholder="Enter reported outages"></ion-input>
+                        <ion-input label="Reported outages" label-placement="stacked" fill="outline" placeholder="Enter reported outages" :value="instal.fault"></ion-input>
                         <ion-label>Link to test procedure:</ion-label>
                         <router-link to="">
                             PDF document
                         </router-link>
-                        <ion-textarea label="Comments" label-placement="stacked" fill="outline" placeholder="Enter a comment"></ion-textarea>
-                        <ion-checkbox label-placement="end" :checked="inspection.installations.approved">Approved</ion-checkbox>
+                        <ion-textarea label="Comments" label-placement="stacked" fill="outline" placeholder="Enter a comment" :value="instal.comments"></ion-textarea>
+                        <ion-checkbox label-placement="end" :checked="inspection.installations.approved" :value="instal.approved">Approved</ion-checkbox>
                     </fieldset>
-                    <fieldset>
-                        <legend>Inventory modifications</legend>
+                    <h2>Inventory modifications</h2>
+                    <fieldset v-for="mod in inspection.modifications" :key="mod.id">
+                        <legend>Modification {{ mod.id }}</legend>
                         <ion-label>Existing situation and already documented modifications:</ion-label>
                         <router-link to="">
                             PDF document
                         </router-link>
-                        <ion-input label="Location of modification found" label-placement="stacked" fill="outline" placeholder="Enter the location"></ion-input>
-                        <ion-select label="Modification done by" label-placement="stacked" fill="outline" placeholder="Select the executor">
+                        <ion-input label="Location of modification found" label-placement="stacked" fill="outline" placeholder="Enter the location" :value="mod.location"></ion-input>
+                        <ion-select label="Modification done by" label-placement="stacked" fill="outline" placeholder="Select the executor" :value="mod.doneBy">
                             <ion-select-option value="tenant">Tenant</ion-select-option>
                             <ion-select-option value="contractor">Contractor</ion-select-option>
                             <ion-select-option value="unknown">Unknown</ion-select-option>
                         </ion-select>
-                        <ion-textarea label="Modification description" label-placement="stacked" fill="outline" placeholder="Enter a description of the modification"></ion-textarea>
-                        <ion-select label="Action to be taken" label-placement="stacked" fill="outline" placeholder="Select an action">
+                        <ion-textarea label="Modification description" label-placement="stacked" fill="outline" placeholder="Enter a description of the modification" :value="mod.description"></ion-textarea>
+                        <ion-select label="Action to be taken" label-placement="stacked" fill="outline" placeholder="Select an action" :value="mod.action">
                             <ion-select-option value="accept">Accept</ion-select-option>
                             <ion-select-option value="inspect">Have it inspected</ion-select-option>
                             <ion-select-option value="remove">Have it removed</ion-select-option>
-                            <ion-select-option value="adjust">have it adjusted and inspected</ion-select-option>
+                            <ion-select-option value="adjust">Have it adjusted and inspected</ion-select-option>
                         </ion-select>
-                        <ion-textarea label="Comments" label-placement="stacked" fill="outline" placeholder="Enter a comment"></ion-textarea>
+                        <ion-textarea label="Comments" label-placement="stacked" fill="outline" placeholder="Enter a comment" :value="mod.comments"></ion-textarea>
                     </fieldset>
                     <ion-button>
                         Save inspection
@@ -163,7 +168,7 @@
     form {
         display: flex;
         flex-direction: column;
-        gap: 2rem;
+        gap: 1rem;
     }
     @media screen and (min-width: 30rem) {
         form {
