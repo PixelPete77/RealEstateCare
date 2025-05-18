a0e72c25225c437ff45fadb2c6e00aa134ebbc3c
Pieter Bas Lugthart <pieterbas@80db.nl>
2025-05-18 13:24:18 +0200

    Separate fetching users and checking credentials so we can display correct error messages

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,18 +3,24 @@ import axios from "axios";
 const url = 'https://my-json-server.typicode.com/PixelPete77/RealEstateCare/users';
 
 export const authUser = async (username, password) => {
+    let users = [];
+
+    // Fetch the users from our fake database
     try {
-        // Fetch the users from our fake database
-        const response = await axios.get(url);
-        // Since we don't need to specify if the user was not found or if the password did not match, 
-        // we can check if the user exists and the password is correct at the same time 
-        const user = response.data.find(user => user.username === username && user.password === password);
-        // Throw an error if the user is not found or the password does not match, else return the user
-        if (!user) {
-            throw new Error('Invalid username or password');
-        }
-        return user;
+        const response = await axios.get(url); 
+        users = response.data;  
     } catch (error) {
-        throw new Error(`Failed to fetch users: ${error.message}`);
+        throw new Error(`Failed to fetch users`);
     }
+
+    // Since we don't need to specify if the user was not found or if the password did not match, 
+    // we can check if the user exists and the password is correct at the same time 
+    const user = users.find(user => user.username === username && user.password === password);
+
+    // Throw an error if the user is not found or the password does not match
+    if (!user) {
+        throw new Error('Invalid username or password');
+    }
+    // Return the user
+    return user;
 }
