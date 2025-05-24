3b3f6148c4aa0bdc3eff61434b3bbdd739a87f45
Pieter Bas Lugthart <pieterbas@80db.nl>
2025-05-24 19:23:07 +0200

    Router: specify which pages need authentication. Redirect to the Login page if not.

--- src/router/index.js
+++ src/router/index.js
@@ -11,31 +11,31 @@ const routes = [
     path: '/completed',
     name: 'Completed inspections',
     component: () => import('@/views/CompletedInspectionsPage.vue'),
-    meta: { layout: 'default', title: 'Completed inspections' },
+    meta: { layout: 'default', requiresAuth: true, title: 'Completed inspections' },
   },
   {
     path: '/help',
     name: 'Help',
     component: () => import('@/views/HelpPage.vue'),
-    meta: { layout: 'default', title: 'Help' },
+    meta: { layout: 'default', requiresAuth: true, title: 'Help' },
   },
   {
     path: '/home',
     name: 'Home',
     component: () => import('@/views/DashboardPage.vue'),
-    meta: { layout: 'default', title: 'Dashboard' },
+    meta: { layout: 'default', requiresAuth: true, title: 'Dashboard' },
   },
   {
     path: '/inspection/:id',
     name: 'inspection',
     component: () => import('@/views/InspectionPage.vue'),
-    meta: { layout: 'default', title: 'Inspection' },
+    meta: { layout: 'default', requiresAuth: true, title: 'Inspection' },
   },
   {
     path: '/knowledge-base',
     name: 'Knowledge base',
     component: () => import('@/views/KnowledgeBasePage.vue'),
-    meta: { layout: 'default', title: 'Knowledge base' },
+    meta: { layout: 'default', requiresAuth: true, title: 'Knowledge base' },
   },
   {
     path: '/login',
@@ -47,13 +47,13 @@ const routes = [
     path: '/scheduled',
     name: 'Scheduled inspections',
     component: () => import('@/views/ScheduledInspectionsPage.vue'),
-    meta: { layout: 'default', title: 'Scheduled inspections' },
+    meta: { layout: 'default', requiresAuth: true, title: 'Scheduled inspections' },
   },
   {
     path: '/settings',
     name: 'Settings',
     component: () => import('@/views/SettingsPage.vue'),
-    meta: { layout: 'default', title: 'Help' },
+    meta: { layout: 'default', requiresAuth: true, title: 'Help' },
   },
   {
     path: '/:pathMatch(.*)*',
@@ -66,6 +66,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, next) => {
+  // Check if session data exists in localStorage. If so, the user is authenticated
+  const session = JSON.parse(localStorage.getItem('session'));
+  const isAuthenticated = session && Date.now() < session.expiresAt;
+  // Check if the route requires authentication and if the user is authenticated
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    return { name: 'Login' }; // Redirect to the login page if not authenticated
+  } else {
+    return true; // Allow navigation to the route
+  }
 });
 
 router.afterEach((to) => {
