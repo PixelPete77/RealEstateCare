d20e3e6b06aa42863c64bec1cfdb5c0c0fbdd340
Pieter Bas Lugthart <pieterbas@80db.nl>
2025-06-04 17:29:53 +0200

    Split fetching completed and scheduled inspections

--- src/stores/inspectionsStore.js
+++ src/stores/inspectionsStore.js
@@ -1,19 +1,39 @@
-import { ref } from "vue";
-import { defineStore } from "pinia";
-import { fetchInspections } from "@/services/inspectionService";
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+import { fetchInspections } from '@/services/inspectionService';
+import { useUserStore } from './userStore';
 
 export const useInspectionsStore = defineStore('inspections', () => {
     // State
     const errors = ref(null);
     const inspections = ref([]);
     const loadingStatus = ref('loading');
+    const userStore = useUserStore();
 
     // Actions
-    const fetchInspectionsData = async () => {
+    const fetchCompletedInspectionsData = async () => {
         loadingStatus.value = 'loading';
+        const inspectorId = userStore.getUser?.id; // Get the user's id from the user store
+
         try {
-            const data = await fetchInspections();
-            inspections.value = data;
+            // Ideally we should only fetch inspections for this user which are not completed
+            // But since we're using a fake database where we can't use 'null' as a parameter in the url, we're fetching all inspections and filtering them
+            const data = await fetchInspections(inspectorId); // Fetch inspections for the specific user
+            inspections.value = data.filter(inspection => inspection.completedDate !== null); // Filter out inspections which have a completed date (the value is not null)
+        } catch (error) {
+            errors.value = error.message;
+        } finally {
+            loadingStatus.value = 'notloading';
+        }
+    }
+
+    const fetchScheduledInspectionsData = async () => {
+        loadingStatus.value = 'loading';
+        const inspectorId = userStore.getUser?.id; // Get the user's id from the user store
+
+        try {
+            const data = await fetchInspections(inspectorId); // Fetch inspections for the specific user
+            inspections.value = data.filter(inspection => inspection.completedDate === null); // Filter out inspections which don't have a completed date
         } catch (error) {
             errors.value = error.message;
         } finally {
@@ -31,7 +51,8 @@ export const useInspectionsStore = defineStore('inspections', () => {
         errors,
         inspections,
         loadingStatus,
-        fetchInspectionsData,
+        fetchCompletedInspectionsData,
+        fetchScheduledInspectionsData,
         getInspectionById
     }
 })
