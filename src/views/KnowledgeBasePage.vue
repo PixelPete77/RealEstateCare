153d164aa030ce6e7db47045587abd93e047a5be
Pieter Bas Lugthart <pieterbas@80db.nl>
2025-06-19 17:38:05 +0200

    Knowledge base show list of documents

--- src/views/KnowledgeBasePage.vue
+++ src/views/KnowledgeBasePage.vue
@@ -1,5 +1,14 @@
 <script setup>
+    import { ref, onMounted } from 'vue';
     import { IonContent, IonPage } from '@ionic/vue';
+
+    const fileList = ref([]);
+
+    const filePaths = import.meta.glob('@/assets/documents/**/*', { as: 'url', eager: true });
+
+    onMounted(() => {
+        fileList.value = Object.values(filePaths);
+    });
 </script>
 
 <template>
@@ -7,6 +16,11 @@
         <ion-content>
             <div class="wrapper">
                 <h1>Knowledge base</h1>
+                <ul>
+                    <li v-for="(file, index) in fileList" :key="index">
+                        <a :href="file" target="_blank">{{ file }}</a>
+                    </li>
+                </ul>
             </div>
         </ion-content>
     </ion-page>
